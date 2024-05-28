37ab061ebf3b8f21d7cd2e0bca859387a3a2d7e2
Itlex <142721284+BurningInHell@users.noreply.github.com>
2024-05-28 12:09:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
-        bot.send_message(message.chat.id, pokemon.get_level())
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
@@ -22,3 +21,18 @@ def get_name(self):
         else:
             return "Pikachu"
 bot.infinity_polling(none_stop=True)
+@bot.message_handler(commands=['attack'])
+def go(message):
+        if message.reply_to_message:
+            pokemon_1 = Pokemon(message.from_user.username)
+            pokemon_2 = Pokemon(message.reply_from_message.from_user.username)
+            pokemon_1.attack(pokemon_2)
+            result = pokemon_1.sttack(pokemon_2)
+            bot.set_message(message.chat.id,result)
+        else:
+             bot.reply_to(message, "Выбери противника")
+@bot.message_handler(commands=['info'])
+def go(message):
+        if message.from_user.username in Pokemon.pokemons.keys():
+            pok = Pokemon.pokemons[message.from_user.username]
+            bot.send_message(message.chat.id, pok.info())
